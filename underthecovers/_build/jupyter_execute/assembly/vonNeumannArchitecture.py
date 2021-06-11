736cb93881b62edd6469a303e7f27d25bf232570
Jonathan Appavoo <jappavoo@bu.edu>
2021-06-11 02:00:25 +0000

    another build

--- underthecovers/_build/jupyter_execute/assembly/vonNeumannArchitecture.py
+++ underthecovers/_build/jupyter_execute/assembly/vonNeumannArchitecture.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # The Von Neumman Architecture
+# # The von Neumman Architecture
 # 
-# Since [assembly code](./intro.ipynb) directly maps to the native machine code of a computer the assembly instructions are all about directly using and controlling the basic functions of the computer in order to get it to do what you want.   Whether that is to search or sort an array of numbers, play a music file or some other task.   
+# As we saw in the [introduction](./intro.ipynb) assembly code directly maps to the native machine code of a computer.  As such the assembly instructions directly allow a programmer to use and control the basic functions of the computer in order to get the computer to do what they want.   Whether that is to search or sort an array of numbers, play a music file or some other task.   
 # 
-# To understand assembly programming we have to learn what the basic parts are of any computer and how they work so we can understand what the assembly instructions allow us to do.    This is why understanding how computers and software work are one in the same as learning assembly programming. 
+# But to understand assembly programming we have to learn what the basic parts are of any computer and how they work so we can understand what the assembly instructions allow us to do.    This is why understanding how computers and software work are one in the same as learning assembly programming. 
 # 
-# Despite the fact that there are many different manufactures of computers they all largly share a basic common structure.  We call the generic components, their organization and the way they interact the architecture.  
+# Despite the fact that there are many different manufactures of computers they all largly share a basic common structure.  We call the generic components, their organization and the way they interact the architecture of a machine.  In our case the common architecture to which most programmable computers are built around is that of the von Neumman Architecture named after [John von Neumann](https://en.wikipedia.org/wiki/John_von_Neumann).
+# > In our journey to understand computers we will be exploring a fascinating story of human innovation and ingenuity.  This story is full of characters, such as John von Neumman, Alan Turning, Admiral Grace Hopper, Ada Love Lace, and many more, that dared to be and think differently.  Not only do we owe a great deal of gratitude to these courageous people, who often risked a lot in suggesting new ways of thinking and doing things, challenging the orthodoxy of their day, we can also draw inspiration in their diversity and bravery. Remember who you are and that your voice matters, tomorrows innovation rests on your actions!  
 # 
 # 
 # 
