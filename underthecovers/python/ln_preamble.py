cfab75599565d8128159e32982b37e0a27c5f825
Jonathan Appavoo <jappavoo@bu.edu>
2021-07-26 19:35:33 +0000

    got urls sorted out so that api calls to create terminals and iframe construction to embed terminals should work both on local server and on jupyterhub servers

--- underthecovers/python/ln_preamble.py
+++ underthecovers/python/ln_preamble.py
@@ -8,21 +8,80 @@
 from IPython.core.display import display, HTML, Markdown, TextDisplayObject, Javascript
 from IPython.display import IFrame
 import ipywidgets as widgets
-import os
+import os, requests
+from notebook.notebookapp import list_running_servers
 
+### BOOTSTRAPPING CODE
+# get server info so that we can make api calls when runing direclty on a
+# jupyter notebook server
+servers=list(list_running_servers())
+info=next(list_running_servers())
+# localhost_url used for explicit calls to my server
+localhost_url=info['url']
+api_url=localhost_url + 'api/'
+api_term_url=api_url + 'terminals'
+api_token=info['token']
+# urls used as relative to my server 
+base_url=info['base_url']
+
+# on the operate-firrst jupyterhub I found that api_token is not set but
+# the JPY_API_TOKEN environment variable or JUPYTERHUB_API_TOKEN
+# should exist
+if not api_token:
+    api_token = os.environ.get('JPY_API_TOKEN')
+
+if not api_token:
+    api_token = os.environ.get('JUPYTERHUB_API_TOKEN')
+    
+if not api_token:
+    print("ERROR: unable to deterimine API token");
+
+
+# get list of current terminals so that we can reuse this if enough exist 
+# otherwise we will create new ones as needed
+r=requests.get(url=api_term_url, headers={'Authorization': 'token ' + api_token})
+TERMINALS=r.json()
+
+def mkTerm():
+    # create a terminal for our editor
+    r=requests.post(url=api_term_url, headers={'Authorization': 'token ' + api_token})
+    c=r.json()
+    return c['name']
+
+# assumes that we will use the first three terminals four our use
+# create standard terminals for organizing editor, build and debugger
+try:
+    EDITORTERM=TERMINALS[0]['name']
+except IndexError:
+    EDITORTERM=mkTerm()
+
+try:
+    BUILDTERM=TERMINALS[1]['name']
+except IndexError:   
+    BUILDTERM=mkTerm()
+
+try:
+    DEBUGGERTERM=TERMINALS[2]['name']
+except IndexError:   
+    DEBUGGERTERM=mkTerm()
+
+
+# FYI:  Don't need this yet so I am commenting out ...
+#       It does work though
 # hack to get the hostname of the current page
 # could not figure out another way to get the
 # hostname for embedded browser windows
 # to use as their url
-from IPython.display import Javascript
-js_code = """
-var ipkernel = IPython.notebook.kernel;
-var stringHostName = window.location.hostname
-var ipcommand = "NB_HOST = " + "'"+stringHostName+"'";
-ipkernel.execute(ipcommand);
-"""
 
-display(Javascript(js_code))
+#from IPython.display import Javascript
+#js_code = """
+#var ipkernel = IPython.notebook.kernel;
+#var stringHostName = window.location.hostname
+#var ipcommand = "NB_HOST = " + "'"+stringHostName+"'";
+#ipkernel.execute(ipcommand);
+#"""
+#
+#display(Javascript(js_code))
 
 # cusomization of ccs to make slides look better 
 display(HTML(
@@ -37,7 +96,7 @@ display(HTML(
     '</style>'
 ))
 
-# Custom function and classes to help with standard slide elements that I use
+# Custom functions and classes to help with standard slide elements that I use
 # NOTE:  I don't know python so this probably all should be rewritten by someone
 #        who knows what they are doing.  
 
@@ -65,7 +124,7 @@ def MkCodeBox(file, lang, html_title, w, h):
 # show Terminal where TERMNAME is one of the terminals we created below
 def showTerm(TERMNAME, name, w, h):
     display(HTML('<b>TERMINAL Window for ' + name + '</b>'))
-    return IFrame('/terminals/' + TERMNAME, w,h)
+    return IFrame(base_url + 'terminals/' + TERMNAME, w,h)
     
 def showET():
     return showTerm(EDITORTERM, "Editor", 1400,600)
@@ -76,57 +135,5 @@ def showBT():
 def showDT():
     return showTerm(DEBUGGERTERM, "Debugger", 1400,800)
 
-# create standard terminals for organizing editor, build and debugger
-import requests
-from notebook.notebookapp import list_running_servers
-# get server info so that we can make api calls 
-servers=list(list_running_servers())
-
-info=next(list_running_servers())
-base_url=info['url']
-api_url=base_url + 'api/terminals'
-api_token=info['token']
-
-# on the operate-firrst jupyterhub I found that api_token is not set but
-# the JPY_API_TOKEN environment variable or JUPYTERHUB_API_TOKEN
-# should exist
-if not api_token:
-    api_token = os.environ.get('JPY_API_TOKEN')
-
-if not api_token:
-    api_token = os.environ.get('JUPYTERHUB_API_TOKEN')
-    
-if not api_token:
-    print("ERROR: unable to deterimine API token");
-
-
-# get list of current terminals so that we can reuse this if enough exist 
-# otherwise we will create new ones as needed
-r=requests.get(url=api_url, headers={'Authorization': 'token ' + api_token})
-TERMINALS=r.json()
-
-try:
-    EDITORTERM=TERMINALS[0]['name']
-except IndexError:
-    # create a terminal for our editor
-    r=requests.post(url=api_url, headers={'Authorization': 'token ' + api_token})
-    c=r.json()
-    EDITORTERM=c['name']
-
-try:
-    BUILDTERM=TERMINALS[1]['name']
-except IndexError:   
-    # create a terminal for running out builds
-    r=requests.post(url=api_url, headers={'Authorization': 'token ' + api_token})
-    c=r.json()
-    BUILDTERM=c['name']
-
-try:
-    DEBUGGERTERM=TERMINALS[2]['name']
-except IndexError:   
-    # create a terminal for running gdb
-    r=requests.post(url=api_url, headers={'Authorization': 'token ' + api_token})
-    c=r.json()
-    DEBUGGERTERM=c['name']
 
 #print("Preamble executed")
