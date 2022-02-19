b527226fde7d954fbf363d320d3b20f6e1f06786
Jonathan Appavoo <jappavoo@bu.edu>
2022-02-19 13:19:56 +0000

    add empty256.s

---
+++ underthecovers/src/empty256.s
@@ -0,0 +1,6 @@
+	.intel_syntax noprefix        # syntax directive
+	.text                         # linker section <directive>
+
+	.global _start                # export symbol _start to linker
+_start:                               # start symbol
+	.fill 256, 1, 0x00            # https://sourceware.org/binutils/docs/as/Fill.html
